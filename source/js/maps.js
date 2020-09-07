6bf84f7bb238deaa2b70694df31eb144b70c7f23
DKukushkin91 <Jame7on@gmail.com>
2020-09-08 03:18:35 +0500

    Исправление ошибок, после проверки

--- source/js/maps.js
+++ source/js/maps.js
@@ -1,25 +1,25 @@
-var mapJs = document.querySelector('.main-contacts__mapps');
-var mapBg = document.querySelector('.main-contacts__background')
+var mapJs = document.querySelector(".main-contacts__mapps");
+var mapBg = document.querySelector(".main-contacts__background")
 
 mapJs.style.display = "none";
 mapBg.style.display = "none";
 
 ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
+  var myMap = new ymaps.Map("map", {
           center: [59.938635, 30.323118],
           zoom: 18
       }, {
-          searchControlProvider: 'yandex#search'
+          searchControlProvider: "yandex#search"
       }),
       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
           '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
       ),
 
       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-          hintContent: 'Cat energy',
+          hintContent: "Cat energy",
       }, {
-          iconLayout: 'default#image',
-          iconImageHref: 'img/mappoint.png',
+          iconLayout: "default#image",
+          iconImageHref: "img/mappoint.png",
           iconImageSize: [57, 53],
           iconImageOffset: [-5, -38]
       });
