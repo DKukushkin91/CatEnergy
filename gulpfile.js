5a69994f714c65c5e952e180a56000d9697f6d5f
DKukushkin91 <Jame7on@gmail.com>
2020-09-08 22:42:19 +0500

    Исправление ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -25,11 +25,11 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
